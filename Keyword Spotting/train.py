230eeb6c062076b0dbe63771bfdadd6bffa6e1ad
ptoidee <remigio_iringan@yahoo.com>
2022-05-31 07:42:00 +0800

    model and video

--- Keyword Spotting/train.py
+++ Keyword Spotting/train.py
@@ -32,7 +32,7 @@ def get_args():
     parser.add_argument('--embed_dim', type=int, default=64, help='embedding dimension')
     parser.add_argument('--num_heads', type=int, default=4, help='num_heads')
     parser.add_argument('--kernel_size', type=int, default=3, help='kernel size')
-    parser.add_argument('--patch_num', type=int, default=8, help='patch_num')
+    parser.add_argument('--patch_num', type=int, default=4, help='patch_num')
 
     # 16-bit fp model to reduce the size
     parser.add_argument("--precision", default=16)
